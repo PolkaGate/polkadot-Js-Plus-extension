647bb5325dfdfefdf80fb06d0f575bbf28629807
Nick-1979 <ekbatanifard@gmail.com>
2022-03-05 00:15:50 +0330

    Update getChainInfo.ts

--- packages/extension-plus/src/util/getChainInfo.ts
+++ packages/extension-plus/src/util/getChainInfo.ts
@@ -19,8 +19,10 @@ async function gChainInfo(_chain: Chain | string): Promise<ChainInfo> {
   // if (!chainName) return null;
 
   const endpoint = allEndpoints.find((e) => (String(e.text)?.toLowerCase() === chainName?.toLowerCase()));
-  // const endpoint = endpoints.find((e) => e.value.includes('onfinality'));
-
+  // const endpoints = allEndpoints.filter((e) => (String(e.text)?.toLowerCase() === chainName?.toLowerCase()));
+  // const onfinalityEndpoint = endpoints.find((e) => e.value.includes('onfinality'));
+  // console.log('endpoints:', endpoints)
+  // const endpoint = onfinalityEndpoint || endpoints[0];
   const wsProvider = new WsProvider(endpoint?.value as string);
 
   const api = await ApiPromise.create({ provider: wsProvider });
