87015e64db27e66a8631d99aec07c78440a05594
Nick-1979 <ekbatanifard@gmail.com>
2022-03-18 20:02:39 +0330

    Update ShortAddress.tsx

--- packages/extension-plus/src/components/ShortAddress.tsx
+++ packages/extension-plus/src/components/ShortAddress.tsx
@@ -7,11 +7,12 @@ import React from 'react';
 interface Props {
   address: string;
   charsCount?: number;
+  fontSize?: number;
 }
 
-export default function ShortAddress ({ address, charsCount = 4 }: Props): React.ReactElement {
+export default function ShortAddress({ address, charsCount = 4, fontSize = 14 }: Props): React.ReactElement {
   return (
-    <span style={{ fontFamily: 'Monospace', fontSize: 14 }}>
+    <span style={{ fontFamily: 'Monospace', fontSize: fontSize }}>
       {address.slice(0, charsCount) + '...' + address.slice(-1 * charsCount)}
     </span>
   );
