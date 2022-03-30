0d0bc7b20b129f493f42f733f51b1790ac5919e6
Nick-1979 <ekbatanifard@gmail.com>
2022-03-30 12:40:25 +0430

    Update InfoTab.spec.tsx
    redesign and add minNominated

--- packages/extension-plus/src/Popup/Staking/InfoTab.spec.tsx
+++ packages/extension-plus/src/Popup/Staking/InfoTab.spec.tsx
@@ -43,9 +43,10 @@ describe('Testing Info component', () => {
     expect(queryByText('Welcome to Staking')).toBeTruthy();
     expect(queryByText('Information you need to know about')).toBeTruthy();
     expect(queryByTestId('info').children.item(3).textContent).toEqual(`Maximum validators you can select:   ${stakingConsts.maxNominations}`);
-    expect(queryByTestId('info').children.item(4).textContent).toEqual(`Minimum ${chainInfo.coin}s to be a staker:   ${stakingConsts.minNominatorBond} ${chainInfo.coin}s`);
-    expect(queryByTestId('info').children.item(5).textContent).toEqual(`Maximum stakers of a validator, who receives rewards:   ${stakingConsts.maxNominatorRewardedPerValidator}`);
+    expect(queryByTestId('info').children.item(4).textContent).toEqual(`Minimum ${chainInfo.coin}s to be a staker:   ${amountToHuman(stakingConsts.minNominatorBond, chainInfo.decimals)} ${chainInfo.coin}s`);
+    expect(queryByTestId('info').children.item(5).textContent).toEqual(`Maximum nominators of a validator, who may receive rewards:   ${stakingConsts.maxNominatorRewardedPerValidator}`);
     expect(queryByTestId('info').children.item(6).textContent).toEqual(`Days it takes to receive your funds back after unstaking:    ${stakingConsts.bondingDuration}  days`);
-    expect(queryByTestId('info').children.item(7).textContent).toEqual(`Minimum ${chainInfo.coin}s that must remain in you account: ${amountToHuman(stakingConsts.existentialDeposit.toString(), chainInfo.decimals)} ${chainInfo.coin}s plus some fees`);
+    expect(queryByTestId('info').children.item(7).textContent).toEqual(`Minimum ${chainInfo.coin}s that must remain in your account: ${amountToHuman(stakingConsts.existentialDeposit.toString(), chainInfo.decimals)} ${chainInfo.coin}`);
+    // expect(queryByTestId('info').children.item(7).textContent).toEqual(`Minimum ${chainInfo.coin}s that must remain in your account: ${amountToHuman(stakingConsts.existentialDeposit.toString(), chainInfo.decimals)} ${chainInfo.coin}s plus some fees`);
   });
 });
