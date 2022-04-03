89398c4ddc07f0bb100b5814c604570179972fec
Kami <ekbatanifard@gmail.com>
2022-04-03 14:39:47 +0430

    Update index.tsx

--- packages/extension-ui/src/Popup/index.tsx
+++ packages/extension-ui/src/Popup/index.tsx
@@ -13,9 +13,8 @@ import uiSettings from '@polkadot/ui-settings';
 
 
 import CrowdLoans from '../../../extension-plus/src/Popup/CrowdLoans';// added for plus
-
-import EnDecrypt from '../../../extension-plus/src/Popup/Encryption';// added for plus
 import Governance from '../../../extension-plus/src/Popup/Governance';// added for plus
+import EnDecrypt from '../../../extension-plus/src/Popup/Encryption';// added for plus
 
 import { ErrorBoundary, Loading } from '../components';
 import { AccountContext, ActionContext, AuthorizeReqContext, MediaContext, MetadataReqContext, SettingsContext, SigningReqContext } from '../components/contexts';
@@ -145,9 +144,8 @@ export default function Popup(): React.ReactElement {
                       <Switch>
 
                         {/* // added for plus */}
-                        {/* <Route path='/auction-crowdloans'>{wrapWithErrorBoundary(<CrowdLoans />, 'auction-crowdloans')}</Route> */}
                         <Route path='/crowdloans/:genesisHash/:address'>{wrapWithErrorBoundary(<CrowdLoans />, 'crowdloans')}</Route>
-                        <Route path='/governance'>{wrapWithErrorBoundary(<Governance />, 'governance')}</Route>
+                        <Route path='/governance/:genesisHash/:address'>{wrapWithErrorBoundary(<Governance />, 'governance')}</Route>
                         <Route path='/endecrypt/:address'>{wrapWithErrorBoundary(<EnDecrypt />, 'encrypt-decrypt')}</Route>
 
                         <Route path='/auth-list'>{wrapWithErrorBoundary(<AuthList />, 'auth-list')}</Route>
