9fd0fd8437150e55acdfceebc52e04ce613745f7
Nick-1979 <ekbatanifard@gmail.com>
2022-04-08 14:50:08 +0430

    Update index.tsx

--- packages/extension-ui/src/Popup/Accounts/index.tsx
+++ packages/extension-ui/src/Popup/Accounts/index.tsx
@@ -7,8 +7,9 @@ import React, { useCallback, useContext, useEffect, useMemo, useState } from 're
 import styled from 'styled-components';
 
 import { AccountWithChildren } from '@polkadot/extension-base/background/types';
-import { AccountsStore } from '@polkadot/extension-base/stores'; // added for plus
 import getNetworkMap from '@polkadot/extension-ui/util/getNetworkMap';
+
+import { AccountsStore } from '@polkadot/extension-base/stores'; // added for plus
 import keyring from '@polkadot/ui-keyring'; // added for plus
 import { cryptoWaitReady } from '@polkadot/util-crypto'; // added for plus
 
