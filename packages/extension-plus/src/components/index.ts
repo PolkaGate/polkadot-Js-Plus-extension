f6f308c856b41e32915d8811fa9376446fef0a9f
Nick-1979 <ekbatanifard@gmail.com>
2022-04-16 08:18:08 +0430

    Update index.ts

--- packages/extension-plus/src/components/index.ts
+++ packages/extension-plus/src/components/index.ts
@@ -14,5 +14,6 @@ export { default as Participator } from './Participator';
 export { default as Password } from './Password';
 export { default as Popup } from './Popup';
 export { default as ShowBalance } from './ShowBalance';
+export { default as ShowBalance2 } from './ShowBalance2';
 export { default as ShowValue } from './ShowValue';
 export { default as ShortAddress } from './ShortAddress';
