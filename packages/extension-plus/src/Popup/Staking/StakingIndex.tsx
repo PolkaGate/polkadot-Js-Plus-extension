915dd4d7f80a8df2f8846cdc318f0381529c07cf
Kami <ekbatanifard@gmail.com>
2022-05-09 11:26:23 +0430

    Update StakingIndex.tsx

--- packages/extension-plus/src/Popup/Staking/StakingIndex.tsx
+++ packages/extension-plus/src/Popup/Staking/StakingIndex.tsx
@@ -82,7 +82,7 @@ export default function StakingIndex ({ account, api, chain, ledger, redeemable,
           </Grid>
 
           <Grid color={grey[500]} container justifyContent='center' sx={{ fontSize: 14, fontWeight: 500, px: 2 }}>
-            {t('Stakers (delegators) with a small amount of tokens can pool their funds together and act as a single nominator. The earning of the pool are split pro rata to a delegator\'s stake in the bonded pool.')}
+            {t('Stakers (delegators) with a small amount of tokens can pool their funds together and act as a single nominator. The earnings of the pool are split pro rata to a delegator\'s stake in the bonded pool.')}
           </Grid>
         </Paper>
       </Grid>
