ef09dd2526197d28ea72c0ce81323b0fd31e0662
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-06-11 14:33:50 +0430

    Update signAndSend.ts

--- packages/extension-plus/src/util/api/signAndSend.ts
+++ packages/extension-plus/src/util/api/signAndSend.ts
@@ -9,7 +9,7 @@ import { ISubmittableResult } from '@polkadot/types/types';
 
 import { TxInfo } from '../plusTypes';
 
-export async function signAndSend(
+export async function signAndSend (
   api: ApiPromise,
   submittable: SubmittableExtrinsic<'promise', ISubmittableResult>,
   _signer: KeyringPair,
