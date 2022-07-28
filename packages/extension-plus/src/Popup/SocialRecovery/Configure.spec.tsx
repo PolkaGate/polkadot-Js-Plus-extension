941e8ac78e92c60cfbc73ef80066dae99f9f88a7
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-07-29 01:49:08 +0430

    Update Configure.spec.tsx

--- packages/extension-plus/src/Popup/SocialRecovery/Configure.spec.tsx
+++ packages/extension-plus/src/Popup/SocialRecovery/Configure.spec.tsx
@@ -3,15 +3,15 @@
 
 import '@polkadot/extension-mocks/chrome';
 
-import { fireEvent, Matcher, render } from '@testing-library/react';
-import getChainInfo from '../../util/getChainInfo';
-import { ChainInfo, nameAddress, RecoveryConsts, Rescuer } from '../../util/plusTypes';
+import { cleanup, render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { BN } from '@polkadot/util';
 
-import { chain, makeShortAddr, validatorsIdentities as accountWithId, validatorsName as accountWithName } from '../../util/test/testHelper';
-
+import getChainInfo from '../../util/getChainInfo';
+import { ChainInfo, nameAddress, RecoveryConsts, Rescuer } from '../../util/plusTypes';
+import { chain, validatorsIdentities as accountWithId, validatorsName as accountWithName } from '../../util/test/testHelper';
 import Configure from './Configure';
 
 jest.setTimeout(240000);
@@ -19,7 +19,7 @@ ReactDOM.createPortal = jest.fn((modal) => modal);
 
 let chainInfo: ChainInfo;
 let recoveryConsts: RecoveryConsts;
-const rescuer: Rescuer = {
+const rescuerValue: Rescuer = {
   accountId: accountWithId[1].accountId,
   option: {
     created: new BN('11907021'),
@@ -28,11 +28,13 @@ const rescuer: Rescuer = {
   }
 };
 const addresesOnThisChain: nameAddress[] = [accountWithName[0], accountWithName[1], accountWithName[2]];
-let status = [undefined, 'makeRecoverable', 'removeRecovery', 'closeRecovery'];
+const setConfigureModalOpen = () => undefined;
+const recoveryInfoStates = [undefined, null];
+const rescuerStates = [undefined, null, rescuerValue];
 
 describe('Testing the Configure component', () => {
   beforeAll(async () => {
-    chainInfo = await getChainInfo('kusama') as ChainInfo;
+    chainInfo = await getChainInfo('westend') as ChainInfo;
 
     recoveryConsts = {
       configDepositBase: chainInfo.api.consts.recovery.configDepositBase as unknown as BN,
@@ -40,25 +42,47 @@ describe('Testing the Configure component', () => {
       maxFriends: chainInfo.api.consts.recovery.maxFriends.toNumber() as number,
       recoveryDeposit: chainInfo.api.consts.recovery.recoveryDeposit as unknown as BN
     };
+
+    await chainInfo.api.query.recovery.recoverable(accountWithId[0].accountId).then((r) => {
+      recoveryInfoStates.push(r.unwrap());
+    });
   });
 
-  test('Checking the existence of the element While ', () => {
-    const { debug } = render(
-      <Configure
-        account={accountWithId[1]}
-        api={chainInfo.api}
-        chain={chain('kusama')}
-        rescuer={rescuer}
-        setConfigureModalOpen={undefined}
-        showConfigureModal={true}
-        accountsInfo={accountWithId}
-        addresesOnThisChain={addresesOnThisChain}
-        recoveryConsts={recoveryConsts}
-        recoveryInfo={null} // Condition to make recoverable modal open 
-        recoveryStatus={status[0]} // Condition to open RecoveryChecking component
-      />
-    );
+  test('Testing RecoveryChecking component', () => {
+    for (const recoveryInfo of recoveryInfoStates) {
+      for (const rescuer of rescuerStates) {
+        console.log('recoveryInfoStates:', recoveryInfoStates.length)
+        const { queryByText } = render(
+          <Configure
+            account={accountWithId[1]} // don't care for now
+            accountsInfo={accountWithId} // don't care
+            addresesOnThisChain={addresesOnThisChain} // don't care
+            api={chainInfo.api} // don't care for now
+            chain={chain('kusama')}
+            recoveryConsts={recoveryConsts} // don't care
+            recoveryInfo={recoveryInfo} // important --> undefined - null - value
+            rescuer={rescuer} // important --> undefined - null -value
+            setConfigureModalOpen={setConfigureModalOpen} // don't care
+            showConfigureModal={true} // don't care
+          />
+        );
 
-    debug(undefined, 30000);
+        if (recoveryInfo === undefined) {
+          expect(queryByText('Checking if the account is recoverable')).toBeTruthy();
+        } else if (recoveryInfo === null) {
+          expect(queryByText('Make recoverable')).toBeTruthy();
+        } else {
+          if (rescuer === undefined) {
+            expect(queryByText('Checking if a malicious rescuer is recovering your account')).toBeTruthy();
+          } else if (rescuer === null) {
+            expect(queryByText('Remove recovery')).toBeTruthy();
+          } else {
+            expect(queryByText('Close recovery')).toBeTruthy();
+          }
+        }
+
+        cleanup();
+      }
+    }
   });
 });
