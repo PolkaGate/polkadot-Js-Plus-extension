623acce01ced1a480a18d2a8896fce00e9455d89
Nick-1979 <ekbatanifard@gmail.com>
2022-09-05 00:42:33 +0430

    Update Identity.tsx

--- packages/extension-plus/src/components/Identity.tsx
+++ packages/extension-plus/src/components/Identity.tsx
@@ -31,8 +31,8 @@ interface Props {
 
 function Identity({ accountInfo, address, api, chain, iconSize = 24, name, showAddress = false, showSocial = true, title = '', totalStaked = '' }: Props): React.ReactElement<Props> {
   const [info, setInfo] = useState<DeriveAccountInfo | undefined>();
-  const [hasSocial, setHasSocial] = useState<boolean | undefined>();
-  const [judgement, setJudgement] = useState<string | undefined>();
+  const [judgement, setJudgement] = useState<string | undefined | null>();
+  const hasSocial = !!(info?.identity?.twitter || info?.identity?.web || info?.identity?.email);
 
   useEffect(() => {
     if (accountInfo) {
@@ -49,8 +49,6 @@ function Identity({ accountInfo, address, api, chain, iconSize = 24, name, showA
   }, [address, accountInfo, api, name]);
 
   useEffect(() => {
-    setHasSocial(!!(info?.identity?.twitter || info?.identity?.web || info?.identity?.email));
-
     // to check if the account has a judgement to set a verified green check
     setJudgement(info?.identity?.judgements && JSON.stringify(info?.identity?.judgements).match(/reasonable|knownGood/gi));
   }, [info]);
