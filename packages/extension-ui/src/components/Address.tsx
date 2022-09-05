e5aae54f8ffb156251818bd1fb65de7601bc6afb
Kami <ekbatanifard@gmail.com>
2022-09-05 14:08:33 +0430

    change faQrcode to faSitemap

--- packages/extension-ui/src/components/Address.tsx
+++ packages/extension-ui/src/components/Address.tsx
@@ -10,7 +10,7 @@ import type { ThemeProps } from '../types';
 
 import { faUsb } from '@fortawesome/free-brands-svg-icons';
 import { faCopy, faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
-import { faCodeBranch, faQrcode } from '@fortawesome/free-solid-svg-icons';
+import { faCodeBranch, faSitemap } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
@@ -190,7 +190,7 @@ function Address({ actions, address, children, className, genesisHash, isExterna
             : (
               <FontAwesomeIcon
                 className='externalIcon'
-                icon={faQrcode}
+                icon={faSitemap}
                 title={t('external account')}
               />
             )
