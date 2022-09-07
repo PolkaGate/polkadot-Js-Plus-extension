4884975d9d026d2831246533709daac76e299a30
Kami <ekbatanifard@gmail.com>
2022-09-07 16:21:08 +0430

    Add manage proxies index

--- packages/extension-plus/src/Popup/ManageProxies/index.tsx
+++ packages/extension-plus/src/Popup/ManageProxies/index.tsx
@@ -9,7 +9,7 @@
 import type { ThemeProps } from '../../../../extension-ui/src/types';
 
 import { Container, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material';
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState, useMemo } from 'react';
 import styled from 'styled-components';
 
 import { AccountsStore } from '@polkadot/extension-base/stores';
@@ -19,15 +19,18 @@ import useGenesisHashOptions from '@polkadot/extension-ui/hooks/useGenesisHashOp
 import keyring from '@polkadot/ui-keyring';
 import { cryptoWaitReady, decodeAddress, encodeAddress } from '@polkadot/util-crypto';
 
-import { AccountContext, ActionContext } from '../../../../extension-ui/src/components/contexts';
+import { AccountContext, ActionContext, SettingsContext } from '../../../../extension-ui/src/components/contexts';
 import useTranslation from '../../../../extension-ui/src/hooks/useTranslation';
 import { createAccountExternal, getMetadata } from '../../../../extension-ui/src/messaging';
 import { Header } from '../../../../extension-ui/src/partials';
-import { Progress, ShortAddress } from '../../components';
+import { Identity, Progress, ShortAddress } from '../../components';
 import { useApi, useEndpoint } from '../../hooks';
-import { NameAddress, Proxy } from '../../util/plusTypes';
+import { AddressState, NameAddress, Proxy } from '../../util/plusTypes';
 import AddressTextBox from './AddressTextBox';
 import { grey } from '@mui/material/colors';
+import useMetadata from '@polkadot/extension-ui/hooks/useMetadata';
+import { useParams } from 'react-router';
+import { getFormattedAddress } from '../../util/plusUtils';
 
 interface Props extends ThemeProps {
   className?: string;
@@ -40,126 +43,57 @@ interface DropdownOption {
 
 export default function ManageProxies({ className }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
+  const { address, genesisHash } = useParams<AddressState>();
   const { accounts } = useContext(AccountContext);
-  const onAction = useContext(ActionContext);
+  const settings = useContext(SettingsContext);
 
-  const [addresesOnThisChain, setAddresesOnThisChain] = useState<NameAddress[]>([]);
-  const [realAddress, setRealAddress] = useState<string | undefined>();
-  const [chain, setChain] = useState<Chain>();
-  const [name, setName] = useState<string | undefined>();
-  const [proxies, setProxies] = useState<Proxy[] | undefined>();
-
-  const endpoint = useEndpoint(accounts, realAddress, chain);
+  const chain = useMetadata(genesisHash, true);
+  const endpoint = useEndpoint(accounts, address, chain);
   const api = useApi(endpoint);
-  const genesisOptions = useGenesisHashOptions();
-
-  const handleAlladdressesOnThisChain = useCallback((prefix: number): void => {
-    const allAddresesOnSameChain = accounts.reduce(function (result: NameAddress[], acc): NameAddress[] {
-      const publicKey = decodeAddress(acc.address);
-
-      result.push({ address: encodeAddress(publicKey, prefix), name: acc?.name });
-
-      return result;
-    }, []);
-
-    setAddresesOnThisChain(allAddresesOnSameChain);
-  }, [accounts]);
-
-  const isAvailable = useCallback((address: string): NameAddress => addresesOnThisChain?.find((a) => a.address === address), [addresesOnThisChain]);
+  const [proxies, setProxies] = useState<Proxy[] | undefined>();
+  const [proxyInfo, setProxyInfo] = useState<DeriveAccountInfo[] | undefined>();
 
   useEffect(() => {
-    // eslint-disable-next-line no-void
-    void cryptoWaitReady().then(() => {
-      keyring.loadAll({ store: new AccountsStore() });
-    }).catch(console.error);
-  }, []);
+    console.log('address', address);
+    console.log('genesisHash', genesisHash);
+    console.log('settings', settings);
+  }, [address, genesisHash, settings]);
+
+  const formatted = useMemo(() => address && chain && settings && getFormattedAddress(address, chain, settings.prefix), [address, chain, settings]);
 
   useEffect(() => {
-    chain?.ss58Format && handleAlladdressesOnThisChain(chain.ss58Format);
-  }, [chain, handleAlladdressesOnThisChain]);
-
-  useEffect(() => {
-    (!realAddress || !chain) && setProxies(undefined);
-  }, [realAddress, chain]);
-
-  useEffect(() => {
-    realAddress && api && api.query.proxy?.proxies(realAddress).then((proxies) => {
+    formatted && api && api.query.proxy?.proxies(formatted).then((proxies) => {
       setProxies(JSON.parse(JSON.stringify(proxies[0])));
-      console.log('proxies:', JSON.parse(JSON.stringify(proxies)));
+      console.log('proxies:', JSON.parse(JSON.stringify(proxies[0])));
     });
-  }, [api, chain, realAddress]);
+  }, [api, chain, formatted]);
 
-  const _onChangeGenesis = useCallback((genesisHash?: string | null): void => {
-    setProxies(undefined);
-    genesisHash && getMetadata(genesisHash, true).then(setChain).catch((error): void => {
-      console.error(error);
+  useEffect(() => {
+    if (!proxies?.length) {
+      return;
+    }
+
+    const proxyInfo = proxies.map((proxy) => {
+      const mayBeFound = accounts.find((acc) => {
+        const formattedAcc = getFormattedAddress(acc.address, chain, settings.prefix);
+        if (formattedAcc === proxy.delegate) {
+          return acc;
+        }
+      });
+
+      return { accountId: proxy.delegate, nickname: mayBeFound?.name };
     });
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    name && realAddress && chain?.genesisHash && createAccountExternal(name, realAddress, chain.genesisHash)
-      .then(() => onAction('/'))
-      .catch((error: Error) => console.error(error));
-  }, [chain?.genesisHash, name, onAction, realAddress]);
-
-  const PSelect = ({ defaultValue, label, onChange, options }: { defaultValue: string | undefined, onChange?: (value: string) => void, options: DropdownOption[], label: string }) => {
-    const _onChange = useCallback(
-      ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) =>
-        onChange && onChange(value.trim()),
-      [onChange]
-    );
-
-    return (
-      <FormControl
-        // variant='standard'
-        sx={{ width: '100%' }}>
-        <InputLabel
-          htmlFor='selectChain'
-        // sx={{ transformOrigin: 'left bottom', fontWeight: 300, letterSpacing: '-0.015em' }}
-        >
-          {label}
-        </InputLabel>
-        <Select
-          defaultValue={defaultValue}
-          id='selectChain'
-          label={label}
-          // input={<BootstrapInput />}
-          onChange={_onChange}
-        // sx={{ width: '100%', height: 31 }}
-        // value={defaultValue}
-        >
-          {options.map(({ text, value }): React.ReactNode => (
-            <MenuItem key={value} sx={{ fontSize: '14px', fontWeight: 300, letterSpacing: '-0.015em' }} value={value}>
-              {text}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    )
-  };
+    setProxyInfo(proxyInfo);
+  }, [accounts, chain, formatted, proxies, settings?.prefix]);
+  console.log('proxyInfo:', proxyInfo);
 
   return (
     <>
       <Header showAdd showBackArrow showSettings smallMargin text={t<string>('Manage Proxies')} />
       <Container sx={{ pt: '10px', px: '30px' }}>
-        <AddressTextBox addresesOnThisChain={addresesOnThisChain} address={realAddress} chain={chain} label={t('Real account')} setAddress={setRealAddress} />
-        <Grid item py='20px' xs>
-          <PSelect defaultValue={chain?.genesisHash} label={'Select the chain'} onChange={_onChangeGenesis} options={genesisOptions} />
-        </Grid>
-        <TextField
-          autoFocus
-          color='warning'
-          fullWidth
-          helperText={t('Enter a name for your real account')}
-          label={t('Name')}
-          name='name'
-          onChange={() => setName(event.target.value)}
-          sx={{ pb: '20px' }}
-          variant='outlined'
-        />
-        <Grid container item sx={{ fontWeight: 600, bgcolor: grey[200], borderRadius: '10px' }}>
-          <Grid item xs={4}>
-            {t('Address')}
+        <Grid container item sx={{ fontSize: 14, fontWeight: 500, bgcolor: grey[200], borderTopRightRadius: '5px', borderTopLeftRadius: '5px', py: '5px', px: '10px' }}>
+          <Grid item xs={6}>
+            {t('identity')}
           </Grid>
           <Grid item xs={3}>
             {t('type')}
@@ -167,49 +101,46 @@ export default function ManageProxies({ className }: Props): React.ReactElement<
           <Grid item xs={2}>
             {t('delay')}
           </Grid>
-          <Grid item xs={3}>
-            {t('available')}
+          <Grid item xs={1}>
+            {t('action')}
           </Grid>
         </Grid>
-        <Grid container item sx={{ pt: '15px', height: 120, overflowY: 'auto' }} xs={12}>
-          {chain && realAddress &&
+        <Grid container item sx={{ borderLeft: '2px solid', borderRight: '2px solid', borderBottom: '2px solid', borderBottomLeftRadius: '30px 10%', borderColor: grey[200], pt: '15px', pl: '10px', height: 180, overflowY: 'auto' }} xs={12}>
+          {!proxies &&
+            <Progress title={t('Loading proxes ...')} pt='20px' />}
+          {proxies?.length && proxyInfo &&
             <>
-              {proxies
-                ? proxies.length
-                  ? proxies.map((proxy, index) => {
-                    const localAccount = isAvailable(proxy.delegate);
+              {proxies.map((proxy, index) => {
+                const info = proxyInfo.find((p) => p.accountId === proxy.delegate);
 
-                    return (
-                      <Grid container item key={index} sx={{ fontSize: 11 }}>
-                        <Grid item xs={4}>
-                          <ShortAddress address={proxy.delegate} fontSize={11} />
-                        </Grid>
-                        <Grid item xs={3}>
-                          {proxy.proxyType}
-                        </Grid>
-                        <Grid item xs={2}>
-                          {proxy.delay}
-                        </Grid>
-                        <Grid item xs={3} >
-                          {!!localAccount ? `Yes (${localAccount.name})` : 'No'}
-                        </Grid>
-                      </Grid>
-                    )
-                  })
-                  : <Grid item pt='10px'>
-                    {t('No proxies found for the entered real account on {{chain}}', { replace: { chain: chain?.name } })}
+                return (
+                  <Grid container item key={index} sx={{ fontSize: 14 }}>
+                    <Grid item xs={6}>
+                      <Identity accountInfo={info} chain={chain} />
+                    </Grid>
+                    <Grid item xs={3}>
+                      {proxy.proxyType}
+                    </Grid>
+                    <Grid item xs={2}>
+                      {proxy.delay}
+                    </Grid>
+                    <Grid item xs={1}>
+                      *
+                    </Grid>
                   </Grid>
-                : <Progress pt={'10px'} title={'Loading proxies ...'} />
+                );
+              })
               }
-            </>}
+            </>
+          }
         </Grid>
         <Grid item sx={{ pt: '30px' }} xs={12}>
           <NextStepButton
-            data-button-action='Add proxy real account'
-            isDisabled={!name || !realAddress | !proxies?.length}
-            onClick={handleAdd}
+            data-button-action='Done'
+          // isDisabled={}
+          // onClick={handleAdd}
           >
-            {t('Add proxy real account')}
+            {t('Done')}
           </NextStepButton>
         </Grid>
 
